05410715f17720703f8f5ef386e0b11fcd0df982
Shang Zhou Xia <shangxzx@gmail.com>
2017-04-01 12:38:49 -0700

    Updates to personal project using alias

--- node/server.js
+++ node/server.js
@@ -25,7 +25,7 @@ var SpeechToText = express.Router();
 router.post('/', function(req, res) {
 
         var text = req.body;
-        console.log(req);
+        console.log(req.url);
         //res.json(req.body);
         
         var request = require("request");
