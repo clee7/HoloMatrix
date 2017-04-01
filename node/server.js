52300d3467d09844557679e76622a962e713b490
Shang Zhou Xia <shangxzx@gmail.com>
2017-04-01 10:52:16 -0700

    initial node files

---
+++ node/server.js
@@ -0,0 +1,56 @@
+
+
+// call the packages we need
+var express    = require('express');        // call express
+var app        = express();                 // define our app using express
+var bodyParser = require('body-parser');
+var http = require('http');
+var request = require("request");
+
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+
+var port = process.env.PORT || 8080;        // set our port
+
+// ROUTES FOR OUR API
+// =============================================================================
+var router = express.Router();              // get an instance of the express Router
+var SpeechToText = express.Router();
+
+
+
+// // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+router.post('/', function(req, res) {
+
+        var url = req.body.url;
+        
+        var request = require("request");
+
+        var options = { method: 'POST',
+        url: 'https://westus.api.cognitive.microsoft.com/face/v1.0/detect',
+        qs: { returnFaceId: 'true', returnFaceLandmarks: 'false' },
+        headers: 
+        { 'postman-token': 'd6f02425-e41e-6b9b-1e81-e1e693f5d347',
+            'cache-control': 'no-cache',
+            'content-type': 'application/json',
+            'ocp-apim-subscription-key': '02f51053e13b481d96e2c2aca0144862' },
+        body: { url: 'https://qph.ec.quoracdn.net/main-qimg-493968e878889ae5dd7927881e87275f-c' },
+        json: true };
+
+        request(options, function (error, response, body) {
+        if (error) throw new Error(error);
+
+        console.log(body);
+        });
+
+});
+
+
+app.use('/api', router);
+
+// START THE SERVER
+// =============================================================================
+app.listen(port);
+console.log('Magic happens on port ' + port);
