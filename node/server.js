6503154ad89662de1f2beb07daf47bfb2a4a8eb0
Shang Zhou Xia <shangxzx@gmail.com>
2017-04-01 11:29:28 -0700

    Updates to personal project using alias

--- node/server.js
+++ node/server.js
@@ -19,7 +19,7 @@ var port = process.env.PORT || 8080;        // set our port
 var router = express.Router();              // get an instance of the express Router
 var SpeechToText = express.Router();
 
-
+//
 
 // // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 router.post('/', function(req, res) {
